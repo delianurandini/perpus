af1f1a396b83765c68726d3030b59c9c23b3c13e
user <151610262@smkassalaambandung.sch.id>
2017-08-03 17:08:00 +0700

    perpus

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    <div class="panel-body">
+                        Selamat Datang di Larapus
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
